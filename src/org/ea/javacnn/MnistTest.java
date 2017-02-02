ea50adf7db7702086ae139c835bf3da1c85a44e5
Daniel Persson <mailto.woden@gmail.com>
2017-02-02 05:07:59 +0100

    Added visualization.

--- src/org/ea/javacnn/MnistTest.java
+++ src/org/ea/javacnn/MnistTest.java
@@ -9,6 +9,7 @@ import org.ea.javacnn.trainers.AdaGradTrainer;
 import org.ea.javacnn.trainers.Trainer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -36,9 +37,13 @@ public class MnistTest {
         Trainer trainer = new AdaGradTrainer(net, 20, 0.001f);
 
         MnistReader mr = new MnistReader("mnist/train-labels-idx1-ubyte", "mnist/train-images-idx3-ubyte");
+        MnistReader mrTest = new MnistReader("mnist/t10k-labels-idx1-ubyte", "mnist/t10k-images-idx3-ubyte");
 
         try {
             long start = System.currentTimeMillis();
+
+            int[] numberDistribution = new int[10];
+            int[] correctPredictions = new int[10];
 
             TrainResult tr = null;
             DataBlock db = new DataBlock(28, 28, 1, 0);
@@ -55,11 +60,63 @@ public class MnistTest {
                         start = System.currentTimeMillis();
                     }
                 }
-                System.err.println("Loss: "+(loss / 60000.0));
+                System.out.println("Loss: "+(loss / 60000.0));
                 mr.reset();
+
+                if(j != 1) {
+                    System.out.println("Last run:");
+                    System.out.println("=================================");
+                    printPredictions(correctPredictions, numberDistribution, mrTest.size());
+                }
+
+                start = System.currentTimeMillis();
+                Arrays.fill(correctPredictions, 0);
+                Arrays.fill(numberDistribution, 0);
+                for(int i=0; i<mrTest.size(); i++) {
+                    db.addImageData(mrTest.readNextImage());
+                    net.forward(db, false);
+                    int correct = mrTest.readNextLabel();
+                    int prediction = net.getPrediction();
+                    if(correct == prediction) correctPredictions[correct]++;
+                    numberDistribution[correct]++;
+                }
+                mrTest.reset();
+                System.out.println("Testing time: " + (System.currentTimeMillis() - start));
+
+                System.out.println("Current run:");
+                System.out.println("=================================");
+                printPredictions(correctPredictions, numberDistribution, mrTest.size());
+                start = System.currentTimeMillis();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void printPredictions(int[] correctPredictions, int[] numberDistribution, int totalSize) {
+        int sumCorrectPredictions = 0;
+        for (int i = 0; i < 10; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Number ");
+            sb.append(i);
+            sb.append(" has predictions ");
+            sb.append(correctPredictions[i]);
+            sb.append("/");
+            sb.append(numberDistribution[i]);
+            sb.append("\t\t");
+            sb.append(((float) correctPredictions[i] / (float) numberDistribution[i]) * 100);
+            sb.append("%");
+            System.out.println(sb.toString());
+            sumCorrectPredictions += correctPredictions[i];
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Total correct predictions ");
+        sb.append(sumCorrectPredictions);
+        sb.append("/");
+        sb.append(totalSize);
+        sb.append("\t\t");
+        sb.append(((float) sumCorrectPredictions / (float)totalSize) * 100);
+        sb.append("%");
+        System.out.println(sb.toString());
     }
 }
